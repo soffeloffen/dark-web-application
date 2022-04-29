ab40b940f8008b2306889078b44412bb52897e2b
anso <asod@itu.dk>
2022-04-29 10:07:35 +0200

    test

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+
+const App = () => {
+
+}
+
+//useEffect to fetch json data
+useEffect(() => {
+  const fetchProducts = async () => {
+    const res = await fetch('http://localhost:3000/products/')
+    const data = await res.json()
+
+    console.log(data)
+  }
+  fetchProducts()
+}, [])
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import {useState, useEffect} from 'react'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
