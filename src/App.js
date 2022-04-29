360339ee1dfedc314e41d15c704262a1e2bd598c
anso <asod@itu.dk>
2022-04-29 10:31:51 +0200

    jh

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-
-const App = () => {
-
-
-=======
 function App() {
   return (
     <h1>Hej</h1>
   );
->>>>>>> 0a75f7456adfcba3377576e04aebf7a9e9b12698
 }
-
-
-
 
 export default App;
