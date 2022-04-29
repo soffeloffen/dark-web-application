8f80a5ddc3eb5fc793d1f14fcd5e1b8d055d7387
anso <asod@itu.dk>
2022-04-29 10:29:45 +0200

    ye

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 //import {useState, useEffect} from 'react'
 import ProductCard from './components/ProductCard';
