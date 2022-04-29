61ede2a0ed4b44036be5029e0d8a3eac2039ddd0
anso <asod@itu.dk>
2022-04-29 15:29:49 +0200

    h

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,15 +1,15 @@
 import Product from './Product'
 
  const Products = ({ products }) => {
-
-    return (
-<>     
-{products.map((product) => (
-    <Product
+    return(
+    <>
+    {products.map((product) => (
+        <Product 
+        key={product.id}
         product={product}
-      />
+        />
     ))}
-     </>
+    </>
     )
 }
 
