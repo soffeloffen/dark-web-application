355672f786a4d34bdb0ac1e6001095bc04d23b1f
anso <asod@itu.dk>
2022-04-29 15:30:02 +0200

    j

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,18 +3,10 @@ import ButtonRemoveFromBasket from './ButtonRemoveFromBasket.js'
 
     
  const Product = ({ product }) => {
-  /*  const showProduct=() => {
-        console.log(product.id)
-    }*/
-
-    return (
-        <div>
-            <p>{product.title}</p>
-        <ButtonAddToBasket productid = { product.id } />  <ButtonRemoveFromBasket productid = { product.id } />
-
-        </div>
-
-    )
+    <div className='product'>
+        <p>{product.title}</p>   
+    <ButtonAddToBasket productid = { product.id } />  <ButtonRemoveFromBasket productid = { product.id } />
+    </div>
 }
 
 export default Product
