49a66c16aeabe6624eb5f7508805b4e0f82f393a
anso <asod@itu.dk>
2022-04-29 15:29:57 +0200

    j

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ const App = () => {
         console.log(data)
         return data
     }
+
          
 return(
     <div className="App">
@@ -29,55 +30,6 @@ return(
     <h2> hey from app.js</h2>
     </div>
     )
-
 }
 
 export default App
-
-/* import { useState, useEffect } from 'react'
-import Products from './components/Products'
-
-
-const App = () => {
-    const [products, setProducts] = useState([])
-
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const res = await fetch('http://localhost:3000/products')
-            const data = await res.json()
-
-            console.log(data)
-        }
-
-        fetchProducts()
-    }, [])
-         
-return(
-    <div className='container'>
-    <Products products = { products } />
-<h1>hey</h1>
-    </div>
-    )
-
-}
-
-export default App
-
- {
-            amount: 0,
-            id: '1',
-            type: 'Drug',
-            title: 'Magic Mushrooms',
-            shortdescription: 'Beautiful, locally grown shrooms',
-            longdescription: 'These magic mushrooms are wild, cultivated mushrooms that contain psilocybin, a naturally-occurring psychoactive and hallucinogenic compound. Psilocybin is considered one of the most well-known psychedelics, and thus of the highest quality on the Dark Web',
-            onsale: false,
-          },
-          {
-            amount: 0,
-            id: '2',
-            type: 'Drug',
-            title: 'Cannabis sativa',
-            shortdescription: 'Pure Cannabis, Common Use',
-            longdescription: 'Cannabis sativa is an annual herbaceous flowering plant indigenous to Eastern Asia, but now of cosmopolitan distribution due to widespread cultivation. It is a recreational drug, which can be used for multiple sclerosis and nerve pain.',
-            'onsale': false,
-          },*/
