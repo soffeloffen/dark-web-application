a8f655b0d323e6a50735400d28b5cadf226f4a16
Sofie Tosti Madsen <sotm@itu.dk>
2022-05-01 14:17:03 +0200

    fs

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,18 +4,23 @@ import "./Navbar.css";
 
 class Navbar extends React.Component {
 
+  handleClick(){
+    
+  }
+  
+
   state = { clicked: false };
 
    render() {
     return (
       <nav className="NavbarItems">
           <h1 className="Navbarlogo">Dark Web</h1>
-        <div onClick= {this.handleClick}>
+        <div>
         </div>
         <ul className={this.state.clicked ? 'nav-menubar-active' : 'nav-menu'}>
           {MenuItems.map((item, index) => {
             return (
-              <li key={index}>
+              <li key={index} id = {item.id}>
                 <a className={item.cName}>
                   {item.title}
                 </a>
