f6149aa6c94b465755cf55e56454030ac3d14675
Sofie Tosti Madsen <sotm@itu.dk>
2022-05-01 16:12:45 +0200

    nav

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,11 +3,14 @@ import { useForm } from "react-hook-form"
 import "./LoginForm.css"
 
 const LoginForm = () => {
+
+  
+
   return (
     <div className='LoginForm'> LoginForm
         <form className='labels'>
-            <input type='text' placeholder='Email' name='User Name' className='labels'></input>
-            <input type='text' placeholder='Password' name='Password' className='labels'></input>
+            <input type='text' placeholder='Email' name='User Name' className='labels' id='username'></input>
+            <input type='text' placeholder='Password' name='Password' className='labels' id='password' ></input>
             <input type='submit' className='labels'></input>
         </form>
     </div>
