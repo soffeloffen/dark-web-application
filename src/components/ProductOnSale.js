fee3d84bf9a05a8038ac8eddaa68e1cc5001d1d8
anso <asod@itu.dk>
2022-05-02 15:13:49 +0200

    h

--- src/components/ProductOnSale.js
+++ src/components/ProductOnSale.js
@@ -3,13 +3,14 @@ import ButtonRemoveFromBasket from './ButtonRemoveFromBasket.js'
 import Slider from 'react-styled-carousel';
 import ReactDOM from 'react-dom'
 import ProductInfo from './ProductInfo'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 let clicked = false;
 let prodid = 0;
 
 const ProductOnSale = ({ product }) => {
+const [currentProdID, setCurrentProdID] = useState(0);
 
 const imageClick = (clickproductid) => {
      console.log('Click!!!! on product id: ' + clickproductid);
@@ -28,34 +29,42 @@ const goBack = () =>{
 
 }
 
- const ProdOnSaleDiv =   
+ const ProdOnSaleDiv =  
+<b>{currentProdID == 0 ?
     <div className='product'> 
        <h4> {product.title}</h4>
        <div className="img__wrap">
        <img className="image" src={product.image} className='cat' />
-       <p className="img__description" onClick={() => imageClick(product.id)}>
+       <p className="img__description" onClick={() => setCurrentProdID(product.id)}>
            {product.shortdescription} <br></br>
            <br></br>
            Click here to get more infomation about this product.
            </p>
-       </div>
-    </div>
+       </div> 
+       </div> : null }</b>
+ 
+
 
 
 const ProductInfo = 
 <div className='product'> 
-    <buttton onClick={() => goBack()}>goBack </buttton>
+    <buttton onClick={() => setCurrentProdID(0)}>goBack </buttton>
     <h1> {product.title} </h1>
+    <img className="image2" src={product.image}/>
+     <h3>The price of this product  {product.price} {product.currency} </h3>
+     <p> 
+         {product.longdescription} </p>
     <ButtonAddToBasket productid = { product.id } />  <ButtonRemoveFromBasket productid = { product.id } />
-
-</div>
+</div> 
 
     return (
         <div className='container'> 
-        <b>{product.id == 2 ?  ProductInfo : ""}</b> <b>{product.onsale==true ?  ProdOnSaleDiv  : ""}</b> 
+        <b>{product.onsale==true && currentProdID==0 ?  ProdOnSaleDiv : product.id == currentProdID && currentProdID !=0 ? ProductInfo : "" }</b> 
          </div>
     )
 }
 
 
 export default ProductOnSale
+//       // <b>{product.id == currentProdID ?  ProductInfo : product.onsale==true && currentProdID==0 ?  ProdOnSaleDiv  : ""}</b> 
+
