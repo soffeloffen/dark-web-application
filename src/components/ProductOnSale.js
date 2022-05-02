49c740a8c50ce3d74a066d0c41e19f366e898c72
anso <asod@itu.dk>
2022-05-02 14:42:49 +0200

    h

--- src/components/ProductOnSale.js
+++ src/components/ProductOnSale.js
@@ -3,14 +3,30 @@ import ButtonRemoveFromBasket from './ButtonRemoveFromBasket.js'
 import Slider from 'react-styled-carousel';
 import ReactDOM from 'react-dom'
 import ProductInfo from './ProductInfo'
+import { useState } from 'react'
 
+
+let clicked = false;
+let prodid = 0;
 
 const ProductOnSale = ({ product }) => {
 
 const imageClick = (clickproductid) => {
      console.log('Click!!!! on product id: ' + clickproductid);
-
+     clicked = true;
+    let prodid = clickproductid
+     console.log(clicked)
+     console.log(prodid)
     }  
+
+const goBack = () =>{
+    clicked = false;
+    prodid = 0;
+    console.log(clicked)
+    console.log(prodid)
+
+
+}
 
  const ProdOnSaleDiv =   
     <div className='product'> 
@@ -25,10 +41,19 @@ const imageClick = (clickproductid) => {
        </div>
     </div>
 
+
+const ProductInfo = 
+<div className='product'> 
+    <buttton onClick={() => goBack()}>goBack </buttton>
+    <h1> {product.title} </h1>
+    <ButtonAddToBasket productid = { product.id } />  <ButtonRemoveFromBasket productid = { product.id } />
+
+</div>
+
     return (
         <div className='container'> 
-        <b>{product.onsale==true ?  ProdOnSaleDiv  : ""}</b>
-        </div>
+        <b>{product.id == 2 ?  ProductInfo : ""}</b> <b>{product.onsale==true ?  ProdOnSaleDiv  : ""}</b> 
+         </div>
     )
 }
 
