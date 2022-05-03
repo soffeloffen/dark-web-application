e22cb7f037f3ca82feb9e3072cd343b382dc194d
Sofie Tosti Madsen <sotm@itu.dk>
2022-05-03 18:49:57 +0200

    de

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -3,14 +3,14 @@ import "./Basket.css";
 import { UserContext } from "./UserContext";
 
 export const Basket = () => {
-    const {value} = useContext(UserContext)
+
 
   return (
     <div>
-        <h1 className="basket-title"> Your Basket </h1>
-        <h1>{value}</h1> 
+      <h1 className="basket-title"> Your Basket </h1>
+      
     </div>
-  )
-}
+  );
+};
 
 export default Basket;
