1ce2754e9daaf25643d22f739d08a711d2409e66
Sofie Tosti Madsen <sotm@itu.dk>
2022-05-06 12:29:54 +0200

    hej

--- src/components/ButtonRemoveFromBasket.js
+++ src/components/ButtonRemoveFromBasket.js
@@ -1,38 +1,55 @@
 import axios from "axios";
 
- const ButtonRemoveFromBasket = ({prodid}) => {
+const ButtonRemoveFromBasket = ({ prodid }) => {
+  //"baskets/:id/products/:prodId"
 
-    const onRemoveFromBasket=()=>{
-        //logs the id of product where 'removed from basket' is clicked
-        console.log("REMOVE FROM BASKET product id: " + prodid)
+  const onRemoveFromBasket = () => {
+    //logs the id of product where 'removed from basket' is clicked
+    console.log("REMOVE FROM BASKET product id: " + prodid);
 
-        const deleteBasket = async () => {
-            const res = await fetch("http://localhost:3000/customers");
-            const data = await res.json();
-            const currentUserId = data[data.length - 1].id;
-            console.log("Current User Id: " + currentUserId);
-            axios.delete("http://localhost:3000/baskets", currentUserId).then((response) => {
-              //Wait for the API to respond - statuscode should be 201 if everything went well
-              console.log(response)
-              if (response.status === 201) {
-                console.log("item created");
-              } else {
-                console.log("Failed with error code + " + response.status);
-              }
-            });
-          }
-          deleteBasket()
-    }
+    const deleteBasket = async () => {
+      const res = await fetch("http://localhost:3000/customers");
+      const data = await res.json();
+      const currentUserId = data[data.length - 1].id;
+      console.log("Current User Id: " + currentUserId);
 
-    return (
-       <button onClick={onRemoveFromBasket} style={{backgroundColor: "red"}}
-       className='btn'> 
-        Remove From Basket
-       </button>
-    )
-}
+      fetch("http://localhost:3000/baskets/" + currentUserId).then(
+        (response) => {
+          response.json().then((data) => {
+            console.log("Basket for current user: ");
+            console.log(data);
+            const temp = JSON.stringify([data.products]);
+            console.log("basket items: " + temp);
+          });
+        }
+      );
 
-export default ButtonRemoveFromBasket
+      axios.delete("http://localhost:3000/baskets/"+currentUserId+"/products/"+prodid).then((response) => {
+        //Wait for the API to respond - statuscode should be 201 if everything went well
+        console.log(response)
+        if (response.status === 201) {
+          console.log("Basket successfully created");
+        } else {
+          console.log("Failed with error code + " + response.status);
+        }
+      });
+
+    };
+    deleteBasket();
+  };
+
+  return (
+    <button
+      onClick={onRemoveFromBasket}
+      style={{ backgroundColor: "red" }}
+      className="btn"
+    >
+      Remove From Basket
+    </button>
+  );
+};
+
+export default ButtonRemoveFromBasket;
 
 //Buttons to put for each product. But Product doesn't work right now
 //<ButtonAddToBasket productid = { product.id } />  <ButtonRemoveFromBasket productid = { product.id } />
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,38 +1,40 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "./UserContext";
-
+import "./HomePage.css";
 
 //take the products array from App.js and convert to lsit of products
 const ProductsOnSale = ({ allproducts }) => {
-const {signedInUser, setSignedInUser} = useContext(UserContext);
-const products = allproducts.map((product) => {
-
-//A div to show title and image - with link to product detail page for that prod id
-const ProdOnSaleDiv =  <div className='container'>
-    <h3>{product.title}</h3>
+  const { signedInUser, setSignedInUser } = useContext(UserContext);
+  const products = allproducts.map((product) => {
+    //A div to show title and image - with link to product detail page for that prod id
+    const ProdOnSaleDiv = (
+      <div className="container">
+        <h3>{product.title}</h3>
         <div>
-        <Link to={`/products/${product.id}`}>{<img className="image" src={product.image} />}</Link>
-     </div>   
-</div>
-    
-//return ProdOnSaleDiv if that product's onsale attribute ==true
-        return (
-            <div key={product.id}>
-              <b>{product.onsale==true? ProdOnSaleDiv :""}</b>
-            </div>
-      );
-    });
-    
-//return to the page, the welcome title and the list of products (which here is only products onsale)
-return (
-            <>
-              <h1>Welcome to the Dark Web {signedInUser}</h1>
-              <h3>Products On Sale !!</h3>
-              {products} 
-            </>
-          );
-    };
-        
+          <Link to={`/products/${product.id}`}>
+            {<img className="image" src={product.image} />}
+          </Link>
+        </div>
+      </div>
+    );
 
-export default ProductsOnSale
+    //return ProdOnSaleDiv if that product's onsale attribute ==true
+    return (
+      <div key={product.id}>
+        <b>{product.onsale == true ? ProdOnSaleDiv : ""}</b>
+      </div>
+    );
+  });
+
+  //return to the page, the welcome title and the list of products (which here is only products onsale)
+  return (
+    <>
+      <h1 className="title">Welcome to the Dark Web {signedInUser}</h1>
+      <h3 className="caption">Products On Sale !!</h3>
+      <div className="products">{products}</div>
+    </>
+  );
+};
+
+export default ProductsOnSale;
